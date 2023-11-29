d74089e38cb2b84869cf81278c5c2250a1141978
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2023-11-29 16:25:53 +0000

    test.java made

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+      System.out.println(50*8);
+      System.out.println("Goodbye World");
+      String person = "John";
+      System.out.println(person);
+      int var = 50;
+      System.out.println(10*var);
+    }
+  }
