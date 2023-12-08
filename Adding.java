bc4d9df7bb8539b358f1a482f7f7cbc43647b9c1
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2023-12-08 14:53:33 +0000

    Made Adding.java

---
+++ Adding.java
@@ -0,0 +1,7 @@
+public class Adding {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("What number would you like to add");
+    }
+  } Adding {
+    
+}
