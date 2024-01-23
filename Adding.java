cb193899871df8bfa18d8113979f7dc48ede710c
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2024-01-23 15:07:18 +0000

    Finished User-input and Adding

--- Adding.java
+++ Adding.java
@@ -1,7 +1,10 @@
 public class Adding {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("What number would you like to add");
-    }
-  } Adding {
-    
+  public static void main(String[] args) {
+    System.out.println(50*8);
+    System.out.println("Goodbye World");
+    String person = "John";
+    System.out.println(person);
+    int var = 50;
+    System.out.println(10*var);
+  }
 }
