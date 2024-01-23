f3a67e4c16b24039333914699595262becfa2b16
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2024-01-23 15:23:32 +0000

    Calculator.java currently not working but fixed input.java bug

--- Input.java
+++ Input.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;  // Import the Scanner class
 
-public class Testing {
+public class Input {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
     System.out.println("Enter username");
