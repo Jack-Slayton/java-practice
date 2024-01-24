7aadfa7737765bf342892570ef457ca1c6d2c330
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2024-01-24 15:35:54 +0000

    NOT FINISHED fibboncai

---
+++ Projects/fibboncai.java
@@ -0,0 +1,22 @@
+package Projects;
+import java.util.Scanner;  // Import the Scanner class
+
+public class fibboncai {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("How many Fibboncai numbers would you like to generate?");
+    int amo = myObj.nextInt();  // Read user input
+    if (amo == 1) {
+        System.out.println("1");
+    } else if (amo == 2) {
+        System.out.println("2");
+    } else {
+        int var1 = amo - 2;
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("2");
+        
+    }
+
+  }
+}
