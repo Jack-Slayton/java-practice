78150f40444086110d1e8a5ef89d2b89982cfb67
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2024-01-29 17:16:15 +0000

    Test!

--- Projects/Array2.java
+++ Projects/Array2.java
@@ -1,15 +1,18 @@
 package Projects;
 import java.util.Scanner;  // Import the Scanner class
 
-public class Array {
+public class Array2 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+    System.out.print("X: ");
     int x_in = input.nextInt();
+    System.out.print("Y: ");
     int y_in = input.nextInt();
-    System.out.print(" 0");
-    for (int y = 0; y <= y_in; y++) {
-      for (int x = 0; x <= x_in; x++) {
+    for (int y = 1; y <= y_in; y++) {
+      // System.err.println("Num: " + y);
+      for (int x = 1; x <= x_in; x++) {
         int output = y*x;
+        
         System.out.print(" " + output);
       }
       System.out.print("\n");
