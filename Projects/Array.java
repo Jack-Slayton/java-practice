19364ea8c3f164888bb9178f14da3a874cbeed04
Jack-Slayton <142333211+Jack-Slayton@users.noreply.github.com>
2024-01-29 15:14:47 +0000

    Work on TimeTables

---
+++ Projects/Array.java
@@ -0,0 +1,15 @@
+package Projects;
+import java.util.Scanner;  // Import the Scanner class
+
+public class Array {
+  public static void main(String[] args) {
+    Integer n = 6;
+    Integer n2 = 6 + 1;
+    Integer num = 0;
+    System.out.println(0);
+    while (num < n2) {
+        num = num + 1; 
+        System.out.println("\r" + num);
+  }
+  }
+}
---
+++ Projects/TimeTables.java
@@ -0,0 +1,24 @@
+    package Projects;
+    import java.util.*;
+
+    public class TimeTables {
+        public static void main(String[] args) {
+            Scanner console = new Scanner(System.in);
+            //System.out.println("/ \ // \\ /// \\\");
+            System.out.println("/ \\ // \\\\ /// \\\\\\");
+            System.out.println("\"This string is in quotes\"\nbut this one isn\'t");
+            System.out.println("///////////////// \n\"\" JAVA IS FUN \"\"\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+            System.out.println("\n\n");
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter n");
+        
+            Integer n = myObj.nextInt();  // Read user input
+            Integer x = n * n;
+            Integer e = n + 1;
+            System.out.println(x);
+            for (int u = 0; u < e; u++) {
+                
+              }  
+
+      }
+    }
